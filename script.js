548c0f80ea3e2fe6f443b4a8930dfbe601b97635
zakariamoustaid <zakaria.moustaid@hotmail.com>
2022-01-30 10:50:16 +0100

    added responsive css

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//language 0 = it; language 1 = en
+
+function sLanguage(selection)
+{
+    if(selection.selectedIndex === 1)
+    {
+        document.querySelector('.it').style.display = 'none';
+        document.querySelector('.en').style.display = 'block';
+    }
+    else
+    {
+        document.querySelector('.it').style.display = 'block';
+        document.querySelector('.en').style.display = 'none';
+    }
+
+}
