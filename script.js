0204a10d3ba23916f83902043b632eab3750f298
zakariamoustaid <zakaria.moustaid@hotmail.com>
2022-02-03 17:31:59 +0100

    major fix

--- script.js
+++ script.js
@@ -19,3 +19,33 @@ function sLanguage(selection)
         document.querySelector('.en').style.display = 'none';
     }
 }
+
+function buttonRed()
+{
+    document.querySelector('.buttonDestroy').style.backgroundColor = 'red';
+    document.querySelector('.explode').style.display = 'block';
+}
+function buttonWhite()
+{
+    document.querySelector('.buttonDestroy').style.backgroundColor = 'white';
+    document.querySelector('.explode').style.display = 'none';
+}
+
+function stopAll()
+{
+    alert('Stoooop!!');
+
+}
+
+function getStarted()
+{
+    let intervalId = setInterval(buttonRed, 100);
+    let intervalId2 = setInterval(buttonWhite, 150);
+
+    setTimeout(function() {
+        alert('Stoooop!!!');
+        clearInterval(intervalId); clearInterval(intervalId2);
+        document.querySelector('.buttonDestroy').style.backgroundColor = 'white';
+        document.querySelector('.explode').style.display = 'none';
+    }, 2500);
+}
